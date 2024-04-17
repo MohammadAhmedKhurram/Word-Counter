63e9aee6179e2ab62820a1464e453cc4a6e91844
MohammadAhmedKhurram <162457856+MohammadAhmedKhurram@users.noreply.github.com>
2024-04-18 00:40:04 +0500

    Word_Converter

---
+++ main.ts
@@ -0,0 +1,22 @@
+#!/usr/bin/env node
+
+import inquirer from 'inquirer';
+import chalk from 'chalk';
+
+console.log(chalk.redBright("Welcome to the Word Counter"));
+
+const answer: {
+    Sentence: string
+} = await inquirer.prompt([
+    {
+        name: "Sentence",
+        type: "input",
+        message: chalk.cyan("Enter your group of words.")
+    }
+]);
+
+const words = answer.Sentence.trim().split(" ");
+
+console.log(words);
+
+console.log(chalk.yellow(`Word count is ${words.length}`));
